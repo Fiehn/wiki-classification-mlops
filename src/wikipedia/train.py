ae8f740e3ab5a2d0f6cab1e56fd0fedec023b9be
Fiehn <rasmuslf@gmail.com>
2025-01-20 19:00:17 +0100

    docker file fixed? data and train prelimary update to test cloud artfiact

--- src/wikipedia/train.py
+++ src/wikipedia/train.py
@@ -1,18 +1,20 @@
-import torch
-import pytorch_lightning as pl
-from torch_geometric.loader import DataLoader
 import os
 import shutil
+
+import pytorch_lightning as pl
+import torch
 import typer
+from data import load_data, load_split_data  # Adjust based on your implementation
 from google.cloud import storage
 from model import NodeLevelGNN
-from data import load_split_data  # Adjust based on your implementation
+from pytorch_lightning.loggers import WandbLogger
+from torch_geometric.loader import DataLoader
 
 # Logging
 import wandb
-from pytorch_lightning.loggers import WandbLogger
 
 app = typer.Typer()
+
 
 def download_from_gcs(bucket_name, source_folder, destination_folder):
     """Download files from a GCS bucket."""
@@ -31,10 +33,11 @@ def download_from_gcs(bucket_name, source_folder, destination_folder):
             print(f"Downloaded {blob.name} to {file_path}")
     return destination_folder
 
+
 @app.command()
 def train(
-    bucket_name: str = typer.Argument(..., help="GCS bucket name for data"),
-    source_folder: str = typer.Argument(..., help="Source folder in GCS bucket"),
+    bucket_name: str = typer.Argument("mlops-proj-group3-bucket", help="GCS bucket name for data"),
+    source_folder: str = typer.Argument("torch_geometric_data", help="Source folder in GCS bucket"),
     local_data_folder: str = typer.Argument("data", help="Local folder to store downloaded data"),
     hidden_channels: int = typer.Option(16, help="Number of hidden channels"),
     hidden_layers: int = typer.Option(2, help="Number of hidden layers"),
@@ -44,7 +47,6 @@ def train(
     batch_size: int = typer.Option(32, help="Batch size"),
     model_checkpoint_callback: bool = typer.Option(True, help="Whether to use model checkpointing"),
 ) -> None:
-    
     # Download data from GCS
     data_path = download_from_gcs(bucket_name, source_folder, local_data_folder)
 
@@ -63,7 +65,7 @@ def train(
     wandb_logger.experiment.log({"test_log": "Wandb is active!"})
 
     # Load the dataset from the downloaded data
-    data_module = load_split_data(root=data_path)
+    data_module = load_data(root=data_path)
 
     c_in = data_module.num_node_features
     c_out = data_module.y.max().item() + 1
@@ -80,10 +82,9 @@ def train(
     callbacks = []
     # Callbacks for early stopping and model checkpointing
     if model_checkpoint_callback:
-        model_checkpoint = pl.callbacks.ModelCheckpoint(save_weights_only=True, 
-                                                        mode="max", 
-                                                        monitor="val_acc", 
-                                                        filename="models/current_best_model")
+        model_checkpoint = pl.callbacks.ModelCheckpoint(
+            save_weights_only=True, mode="max", monitor="val_acc", filename="models/current_best_model"
+        )
         callbacks.append(model_checkpoint)
 
     # Trainer
@@ -99,23 +100,29 @@ def train(
     node_data_loader = DataLoader(data_module, batch_size=batch_size, num_workers=7)
     # Train the model
     trainer.fit(model, node_data_loader, node_data_loader)
-    
+
     # Testing the model in the train loop
     trainer.test(model, node_data_loader)
     # Save the model
     torch.save(model.state_dict(), "models/model.pt")
-    
+
     # Save the model as a W&B artifact
-    artifact = wandb.Artifact('model', type='model', metadata=dict({
-            "Accuracies": trainer.callback_metrics,
-            "hidden_channels": hidden_channels,
-            "hidden_layers": hidden_layers,
-            "dropout": dropout,
-            "lr": lr,
-            "num_epochs": num_epochs,
-            "batch_size": batch_size,
-        }))
-    artifact.add_file('models/model.pt')
+    artifact = wandb.Artifact(
+        "model",
+        type="model",
+        metadata=dict(
+            {
+                "Accuracies": trainer.callback_metrics,
+                "hidden_channels": hidden_channels,
+                "hidden_layers": hidden_layers,
+                "dropout": dropout,
+                "lr": lr,
+                "num_epochs": num_epochs,
+                "batch_size": batch_size,
+            }
+        ),
+    )
+    artifact.add_file("models/model.pt")
     wandb.log_artifact(artifact, aliases=["latest_model"])
 
     # Clean up local data folder
@@ -130,10 +137,8 @@ if __name__ == "__main__":
     app()
 
 
-
-
 ########################################################################################################
-# Old verison 
+# Old verison
 
 # import torch
 # import pytorch_lightning as pl
@@ -160,7 +165,7 @@ if __name__ == "__main__":
 #     batch_size: int = typer.Option(32, help="Batch size"),
 #     model_checkpoint_callback: bool = typer.Option(True, help="Whether to use model checkpointing"),
 # ) -> None:
-    
+
 #     # Initialize WandbLogger
 #     wandb_logger = WandbLogger(
 #         project="wiki_classification",
@@ -191,9 +196,9 @@ if __name__ == "__main__":
 #     callbacks = []
 #     # Callbacks for early stopping and model checkpointing
 #     if model_checkpoint_callback:
-#         model_checkpoint = pl.callbacks.ModelCheckpoint(save_weights_only=True, 
-#                                                         mode="max", 
-#                                                         monitor="val_acc", 
+#         model_checkpoint = pl.callbacks.ModelCheckpoint(save_weights_only=True,
+#                                                         mode="max",
+#                                                         monitor="val_acc",
 #                                                         filename="models/current_best_model")
 #         callbacks.append(model_checkpoint)
 
@@ -210,12 +215,12 @@ if __name__ == "__main__":
 #     node_data_loader = DataLoader(data_module,batch_size=batch_size, num_workers=7)
 #     # Train the model
 #     trainer.fit(model, node_data_loader, node_data_loader)
-    
+
 #     # Testing the model in the train loop
 #     trainer.test(model, node_data_loader)
 #     # Save the model
 #     torch.save(model.state_dict(), "models/model.pt")
-    
+
 #     # Save the model as a W&B artifact
 #     artifact = wandb.Artifact('model', type='model', metadata=dict({
 #             "Accuracies": trainer.callback_metrics,
